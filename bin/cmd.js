4fdf85b77db62781a47b893a000a1182a543cc8d
James Halliday <mail@substack.net>
2014-01-04 22:21:56 -0800

    initial thing that works

---
+++ bin/cmd.js
@@ -0,0 +1,97 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
+var minimist = require('minimist');
+var xrparse = require('xrandr-parse');
+
+var argv = minimist(process.argv.slice(2), {
+    alias: { t: 'target', p: 'primary' }
+});
+if (argv.h || argv.help) return usage(0);
+
+if (argv._[0] === 'mirror') {
+    query(function (err, displays) {
+        if (err) return exit(err);
+        var keys = Object.keys(displays);
+        if (keys.length === 0) return exit('no displays detected');
+        
+        var primary = argv.primary || keys.filter(function (key) {
+            return displays[key].index === 0;
+        })[0];
+        if (!primary) return exit('no primary display detected');
+        
+        var target = argv.target || keys.filter(function (key) {
+            return key !== primary && displays[key].connected;
+        })[0];
+        if (!target) return exit('no target display detected');
+        
+        var dp = displays[primary];
+        if (!dp) return exit('requested primary display not found');
+        var dt = displays[target];
+        if (!dt) return exit('requested target display not found');
+        
+        var pratio = dp.modes[0].width / dp.modes[0].height;
+        var tratio = dt.modes[0].width / dt.modes[0].height;
+        var lmatches = dt.modes.filter(function (mode) {
+            return mode.width <= dp.width && mode.height <= dp.height
+        });
+        var amatches = lmatches.filter(function (mode) {
+            return mode.width / mode.height === tratio;
+        });
+        var tmode = amatches[0] || lmatches[0] || dt.modes[0];
+        
+        var args = [
+            '--output', primary,
+            '--mode', dp.width + 'x' + dp.height,
+            '--output', target,
+            '--mode', tmode.width + 'x' + tmode.height,
+            '--scale', (pratio / tratio) + 'x1'
+        ];
+        spawn('xrandr', args, { stdio: 'inherit' });
+    });
+}
+else if (/^(right|left|top|bottom)$/.test(argv._[0])) {
+    query(function (err, displays) {
+        var keys = Object.keys(displays);
+        if (keys.length === 0) return exit('no displays detected');
+        
+        var primary = argv.primary || keys.filter(function (key) {
+            return displays[key].index === 0;
+        })[0];
+        if (!primary) return exit('no primary display detected');
+        
+        var target = argv.target || keys.filter(function (key) {
+            return key !== primary && displays[key].connected;
+        })[0];
+        if (!target) return exit('no target display detected');
+        
+        var args = [
+            '--output', target, '--auto',
+            '--' + argv._[0] + '-of', primary
+        ];
+        spawn('xrandr', args, { stdio: 'inherit' });
+    });
+}
+else usage(1);
+
+function usage (code) {
+    var rs = fs.createReadStream(__dirname + '/usage.txt');
+    rs.pipe(process.stdout);
+    rs.on('end', function () {
+        if (code !== 0) process.exit(code);
+    });
+}
+
+function query (cb) {
+    exec('xrandr', function (err, stdout) {
+        if (err) cb(err)
+        else cb(null, xrparse(stdout))
+    });
+}
+
+function exit (msg) {
+    console.error(msg);
+    process.exit(1);
+}
---
+++ readme.markdown
@@ -0,0 +1,36 @@
+# xit
+
+configure an external display on linux
+
+xrandr commands are really hard to memorize and graphical tools are frustrating,
+especially when you're setting up a presentation in front of a room full of
+people
+
+# usage
+
+```
+usage:
+
+  xit mirror
+
+    Mirror the current primary resolution to the external display resolution,
+    with changing the current primary resolution or geometry.
+
+  xit right, xit left, xit top, xit bottom
+
+    Set up the display to the right, left, top, or bottom of the primary
+    display.
+
+```
+
+# install
+
+To get the `xit` command, with [npm](https://npmjs.org) do:
+
+```
+npm install -g xit
+```
+
+# license
+
+MIT
